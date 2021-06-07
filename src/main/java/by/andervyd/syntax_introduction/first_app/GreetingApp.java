d0bf5fe52baf621f8d6e9d5649c54b5fc005df81
andervyd <andervyd@gmail.com>
2021-06-07 14:16:20 +0300

    add basic constructions

--- src/main/java/by/andervyd/syntax_introduction/first_app/GreetingApp.java
+++ src/main/java/by/andervyd/syntax_introduction/first_app/GreetingApp.java
@@ -1,6 +1,6 @@
-package by.andervyd.introduction;
+package by.andervyd.syntax_introduction.first_app;
 
-public class FirstApp {
+public class GreetingApp {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
---
+++ src/main/java/by/andervyd/syntax_introduction/first_app/GreetingAppInWindow.java
@@ -0,0 +1,10 @@
+package by.andervyd.syntax_introduction.first_app;
+
+import javax.swing.*;
+
+public class GreetingAppInWindow {
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Hello World!");
+    }
+}
