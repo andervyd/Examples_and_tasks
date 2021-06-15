bbf4a9bda1169fcad03eeb0922e68062e6172fb3
andervyd <andervyd@gmail.com>
2021-06-15 18:29:17 +0300

    add oop: polymorphism

---
+++ src/main/java/by/andervyd/introduction_oop/principles_obj_prog/polymorphism/Auto.java
@@ -0,0 +1,31 @@
+package by.andervyd.introduction_oop.principles_obj_prog.polymorphism;
+
+public class Auto extends Vehicle {
+    private boolean sunRoof;
+
+    public Auto(boolean sunRoof) {
+        this(4, 4, 200, 12, sunRoof);
+    }
+
+    public Auto(int passenger, int wheels, int maxSpeed, int burnUp, boolean sunRoof) {
+        super(passenger, wheels, maxSpeed, burnUp);
+        this.sunRoof = sunRoof;
+    }
+
+    public boolean isSunRoof() {
+        return sunRoof;
+    }
+
+    public void setSunRoof(boolean sunRoof) {
+        this.sunRoof = sunRoof;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto | passenger: " + super.getPassenger()
+                  + ", wheels: " + super.getWheels()
+                  + ", max speed: " + super.getMaxSpeed()
+                  + ", burn up: " + super.getBurnUp()
+                  + ", sunroof: " + sunRoof;
+    }
+}
---
+++ src/main/java/by/andervyd/introduction_oop/principles_obj_prog/polymorphism/VehicleOverrideDemo.java
@@ -0,0 +1,18 @@
+package by.andervyd.introduction_oop.principles_obj_prog.polymorphism;
+
+public class VehicleOverrideDemo {
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle(2, 2, 100, 9);
+        Auto auto = new Auto(2, 4, 180, 12, true);
+
+        Vehicle[] list = {vehicle, auto};
+
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i].toString());
+        }
+        // >> Vehicle | passenger: 2, wheels: 2, max speed: 100, burn up: 9
+        //    Auto | passenger: 2, wheels: 4, max speed: 180, burn up: 12, sunroof: true
+
+    }
+}
