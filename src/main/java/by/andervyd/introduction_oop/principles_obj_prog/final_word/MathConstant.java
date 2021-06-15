86d167dfed12c1caeaf704d9d2cb02e4c2632c7e
andervyd <andervyd@gmail.com>
2021-06-15 18:52:07 +0300

    add oop: word 'final'

---
+++ src/main/java/by/andervyd/introduction_oop/principles_obj_prog/final_word/MathConstant.java
@@ -0,0 +1,6 @@
+package by.andervyd.introduction_oop.principles_obj_prog.final_word;
+
+final class MathConstant {
+    public static final double PI = 3.14159;
+    public static final double E = 2.71828;
+}
---
+++ src/main/java/by/andervyd/introduction_oop/principles_obj_prog/final_word/MathDemo.java
@@ -0,0 +1,11 @@
+package by.andervyd.introduction_oop.principles_obj_prog.final_word;
+
+public class MathDemo {
+    public static void main(String[] args) {
+
+        System.out.println(MathConstant.PI);
+        System.out.println(MathConstant.E);
+        // >> 3.14159
+        //    2.71828
+    }
+}
